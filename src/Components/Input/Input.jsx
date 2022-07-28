2d502957f4d0baade391669363e87fe9e4974352
Conor Higgins <wodowods@gmail.com>
2022-07-28 14:49:20 +0100

    clear input box functionality

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,15 +1,23 @@
+import { createRef } from "react";
 import "./Input.scss";
 
 const Input = ({ value, handleInput, addToDo, updateTDList }) => {
+    let inputRef = createRef();
+    const handleClick = () => {
+        updateTDList();
+        inputRef.current.value = "";
+    };
+
     return (
         <div className="input">
             <input
+                ref={inputRef}
                 type="text"
                 placeholder={value}
                 className="input__textbox"
-                onInput={handleInput}
+                onChange={handleInput}
             />
-            <button className="input__add-button" onClick={updateTDList} />
+            <button className="input__add-button" onClick={handleClick} />
         </div>
     );
 };
