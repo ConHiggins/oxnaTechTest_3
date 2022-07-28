654d3b4f0ef3322dfb8f6fbe629abe7842596280
Conor Higgins <wodowods@gmail.com>
2022-07-28 12:03:15 +0100

    strikethrough function

--- src/Components/ToDo/ToDo.jsx
+++ src/Components/ToDo/ToDo.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import "./ToDo.scss";
 
 const ToDo = ({ value }) => {
+    const [strike, setStrike] = useState(false);
+    const [style, setStyle] = useState("none");
+
+    const handleStrike = () => {
+        setStrike(!strike);
+    };
+
+    const handleStyle = (strike) => {
+        strike ? setStyle("line-through") : setStyle("none");
+    };
+
+    useEffect(() => {
+        handleStyle(strike);
+    }, [strike]);
+
     return (
         <div className="to-do">
-            <input className="to-do__check" type="checkbox" />
-            <p className="to-do__title">{value}</p>
+            <input
+                className="to-do__check"
+                type="checkbox"
+                onChange={handleStrike}
+            />
+            <p className="to-do__title" style={{ textDecoration: style }}>
+                {value}
+            </p>
             <button className="to-do__delete">|||</button>
         </div>
     );
