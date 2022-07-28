5fa76e8831d166235d8159953ae17d92de138403
Conor Higgins <wodowods@gmail.com>
2022-07-28 14:34:55 +0100

    splash animation / new item animation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.scss";
 const Navbar = ({ resetToDos }) => {
     return (
         <div className="nav-bar">
-            <p className="nav-bar__title">My ToDos</p>
+            <p className="nav-bar__title">My Todos</p>
             <button className="nav-bar__reset-button" onClick={resetToDos}>
                 Reset
             </button>
